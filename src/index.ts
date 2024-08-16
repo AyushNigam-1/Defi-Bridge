36bd25d53b2d0d8f14e331b2a91e36fc32a76570
Ayush <ayushnigam843@gmail.com>
2024-08-16 08:24:47 +0530

    Fixed TS File Compilation Issue

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,126 @@
-import { getSchnorrAccount } from '@aztec/accounts/schnorr';
-import { Fr, GrumpkinScalar, createPXEClient } from '@aztec/aztec.js';
-import { Contract } from '@aztec/aztec.js';
-import { TokenBridgeContract, TokenBridgeContractArtifact } from './artifacts/TokenBridge.js';
+import {
+  type AztecAddress,
+  type AztecNode,
+  type DebugLogger,
+  EthAddress,
+  ExtendedNote,
+  type FieldsOf,
+  Fr,
+  Note,
+  type PXE,
+  type SiblingPath,
+  type TxHash,
+  type TxReceipt,
+  type Wallet,
+  computeSecretHash,
+  deployL1Contract,
+  retryUntil,
+} from '@aztec/aztec.js';
+import { type L1ContractAddresses } from '@aztec/ethereum';
+import { sha256ToField } from '@aztec/foundation/crypto';
+import {
+  InboxAbi,
+  OutboxAbi,
+  PortalERC20Abi,
+  PortalERC20Bytecode,
+  TokenPortalAbi,
+  TokenPortalBytecode,
+} from '@aztec/l1-artifacts';
+import { TokenContract } from '@aztec/noir-contracts.js/Token';
+import { TokenBridgeContract } from '@aztec/noir-contracts.js/TokenBridge';
 
-async function main() {
+import {
+  type Account,
+  type Chain,
+  type GetContractReturnType,
+  type HttpTransport,
+  type PublicClient,
+  type WalletClient,
+  getContract,
+  toFunctionSelector,
+} from 'viem';
 
-  const PXE_URL = process.env.PXE_URL || 'http://localhost:8080';
-  const secretKey = Fr.random();
-  const signingPrivateKey = GrumpkinScalar.random();
-  const pxe = createPXEClient(PXE_URL);
+/**
+ * Deploy L1 token and portal, initialize portal, deploy a non native l2 token contract, its L2 bridge contract and attach is to the portal.
+ * @param wallet - the wallet instance
+ * @param walletClient - A viem WalletClient.
+ * @param publicClient - A viem PublicClient.
+ * @param rollupRegistryAddress - address of rollup registry to pass to initialize the token portal
+ * @param owner - owner of the L2 contract
+ * @param underlyingERC20Address - address of the underlying ERC20 contract to use (if none supplied, it deploys one)
+ * @returns l2 contract instance, bridge contract instance, token portal instance, token portal address and the underlying ERC20 instance
+ */
+export async function deployAndInitializeTokenAndBridgeContracts(
+  wallet: Wallet,
+  walletClient: WalletClient<HttpTransport, Chain, Account>,
+  publicClient: PublicClient<HttpTransport, Chain>,
+  rollupRegistryAddress: EthAddress,
+  owner: AztecAddress,
+  underlyingERC20Address?: EthAddress,
+): Promise<{
+  /**
+   * The L2 token contract instance.
+   */
+  token: TokenContract;
+  /**
+   * The L2 bridge contract instance.
+   */
+  bridge: TokenBridgeContract;
+  /**
+   * The token portal contract address.
+   */
+  tokenPortalAddress: EthAddress;
+  /**
+   * The token portal contract instance
+   */
+  tokenPortal: any;
+  /**
+   * The underlying ERC20 contract instance.
+   */
+  underlyingERC20: any;
+}> {
+  if (!underlyingERC20Address) {
+    underlyingERC20Address = await deployL1Contract(walletClient, publicClient, PortalERC20Abi, PortalERC20Bytecode);
+  }
+  const underlyingERC20 = getContract({
+    address: underlyingERC20Address.toString(),
+    abi: PortalERC20Abi,
+    client: walletClient,
+  });
 
-  const wallet = await getSchnorrAccount(pxe, secretKey, signingPrivateKey).waitSetup();
+  // deploy the token portal
+  const tokenPortalAddress = await deployL1Contract(walletClient, publicClient, TokenPortalAbi, TokenPortalBytecode);
+  const tokenPortal = getContract({
+    address: tokenPortalAddress.toString(),
+    abi: TokenPortalAbi,
+    client: walletClient,
+  });
 
-  const deployedContract = await TokenBridgeContract.deploy(
-    wallet, // Wallet instance
-    wallet.getAddress(), // token (AztecAddressLike)
-    '0xYourPortalAddressHere' // portal_address (EthAddressLike)
-  )
-    .send()
-    .deployed();
+  // deploy l2 token
+  const token = await TokenContract.deploy(wallet, owner, 'TokenName', 'TokenSymbol', 18).send().deployed();
 
-  console.log(`Token deployed at ${deployedContract.address.toString()}`);
-  const contract = await Contract.at(deployedContract.address, TokenBridgeContractArtifact, wallet);
+  // deploy l2 token bridge and attach to the portal
+  const bridge = await TokenBridgeContract.deploy(wallet, token.address, tokenPortalAddress).send().deployed();
 
+  if ((await token.methods.admin().simulate()) !== owner.toBigInt()) {
+    throw new Error(`Token admin is not ${owner}`);
+  }
+
+  if (!(await bridge.methods.get_token().simulate()).equals(token.address)) {
+    throw new Error(`Bridge token is not ${token.address}`);
+  }
+
+  // make the bridge a minter on the token:
+  await token.methods.set_minter(bridge.address, true).send().wait();
+  if ((await token.methods.is_minter(bridge.address).simulate()) === 1n) {
+    throw new Error(`Bridge is not a minter`);
+  }
+
+  // initialize portal
+  await tokenPortal.write.initialize(
+    [rollupRegistryAddress.toString(), underlyingERC20Address.toString(), bridge.address.toString()],
+    {} as any,
+  );
+
+  return { token, bridge, tokenPortalAddress, tokenPortal, underlyingERC20 };
 }
-main().catch(err => {
-  console.error(`Error in deployment: ${err}`);
-  process.exit(1);
-});
