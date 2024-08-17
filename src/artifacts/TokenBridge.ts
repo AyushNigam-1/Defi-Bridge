b1f0474dc67fa1f83a5ae861f668fe654f58eae7
Ayush <ayushnigam843@gmail.com>
2024-08-17 08:53:24 +0530

    Deployed Aztec Contract Successfully

--- src/artifacts/TokenBridge.ts
+++ src/artifacts/TokenBridge.ts
@@ -66,14 +66,14 @@ export class TokenBridgeContract extends ContractBase {
   /**
    * Creates a tx to deploy a new instance of this contract.
    */
-  public static deploy(wallet: Wallet, token: AztecAddressLike, portal_address: EthAddressLike) {
+  public static deploy(wallet: Wallet, token: AztecAddressLike) {
     return new DeployMethod<TokenBridgeContract>(Fr.ZERO, wallet, TokenBridgeContractArtifact, TokenBridgeContract.at, Array.from(arguments).slice(1));
   }
 
   /**
    * Creates a tx to deploy a new instance of this contract using the specified public keys hash to derive the address.
    */
-  public static deployWithPublicKeysHash(publicKeysHash: Fr, wallet: Wallet, token: AztecAddressLike, portal_address: EthAddressLike) {
+  public static deployWithPublicKeysHash(publicKeysHash: Fr, wallet: Wallet, token: AztecAddressLike) {
     return new DeployMethod<TokenBridgeContract>(publicKeysHash, wallet, TokenBridgeContractArtifact, TokenBridgeContract.at, Array.from(arguments).slice(2));
   }
 
@@ -152,32 +152,26 @@ TokenNote: {
   /** Type-safe wrappers for the public methods exposed by the contract. */
   public override methods!: {
     
-    /** get_token() */
-    get_token: (() => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
-
     /** exit_to_l1_private(token: struct, recipient: struct, amount: field, caller_on_l1: struct, nonce: field) */
     exit_to_l1_private: ((token: AztecAddressLike, recipient: EthAddressLike, amount: FieldLike, caller_on_l1: EthAddressLike, nonce: FieldLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
 
-    /** get_portal_address() */
-    get_portal_address: (() => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
+    /** constructor(token: struct) */
+    constructor: ((token: AztecAddressLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
 
-    /** claim_private(secret_hash_for_redeeming_minted_notes: field, amount: field, secret_for_L1_to_L2_message_consumption: field) */
-    claim_private: ((secret_hash_for_redeeming_minted_notes: FieldLike, amount: FieldLike, secret_for_L1_to_L2_message_consumption: FieldLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
+    /** get_token() */
+    get_token: (() => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
 
     /** compute_note_hash_and_optionally_a_nullifier(contract_address: struct, nonce: field, storage_slot: field, note_type_id: field, compute_nullifier: boolean, serialized_note: array) */
     compute_note_hash_and_optionally_a_nullifier: ((contract_address: AztecAddressLike, nonce: FieldLike, storage_slot: FieldLike, note_type_id: FieldLike, compute_nullifier: boolean, serialized_note: FieldLike[]) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
 
-    /** exit_to_l1_public(recipient: struct, amount: field, caller_on_l1: struct, nonce: field) */
-    exit_to_l1_public: ((recipient: EthAddressLike, amount: FieldLike, caller_on_l1: EthAddressLike, nonce: FieldLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
+    /** exit_to_l1_public(recipient: struct, amount: field, nonce: field) */
+    exit_to_l1_public: ((recipient: EthAddressLike, amount: FieldLike, nonce: FieldLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
 
-    /** constructor(token: struct, portal_address: struct) */
-    constructor: ((token: AztecAddressLike, portal_address: EthAddressLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
+    /** claim_private(secret_hash_for_redeeming_minted_notes: field, amount: field, secret_for_L1_to_L2_message_consumption: field) */
+    claim_private: ((secret_hash_for_redeeming_minted_notes: FieldLike, amount: FieldLike, secret_for_L1_to_L2_message_consumption: FieldLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
 
-    /** claim_public(to: struct, amount: field, secret: field, message_leaf_index: field) */
-    claim_public: ((to: AztecAddressLike, amount: FieldLike, secret: FieldLike, message_leaf_index: FieldLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
-
-    /** get_portal_address_public() */
-    get_portal_address_public: (() => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
+    /** claim_public(to: struct, amount: field) */
+    claim_public: ((to: AztecAddressLike, amount: FieldLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
   };
 
   
--- src/index.ts
+++ src/index.ts
@@ -1,126 +1,53 @@
+import { getSchnorrAccount } from '@aztec/accounts/schnorr';
+import { getDeployedTestAccountsWallets } from '@aztec/accounts/testing';
 import {
-  type AztecAddress,
-  type AztecNode,
-  type DebugLogger,
-  EthAddress,
   ExtendedNote,
-  type FieldsOf,
   Fr,
+  GrumpkinScalar,
   Note,
   type PXE,
-  type SiblingPath,
-  type TxHash,
-  type TxReceipt,
-  type Wallet,
   computeSecretHash,
-  deployL1Contract,
-  retryUntil,
+  createDebugLogger,
+  createPXEClient,
+  waitForPXE,
 } from '@aztec/aztec.js';
-import { type L1ContractAddresses } from '@aztec/ethereum';
-import { sha256ToField } from '@aztec/foundation/crypto';
-import {
-  InboxAbi,
-  OutboxAbi,
-  PortalERC20Abi,
-  PortalERC20Bytecode,
-  TokenPortalAbi,
-  TokenPortalBytecode,
-} from '@aztec/l1-artifacts';
 import { TokenContract } from '@aztec/noir-contracts.js/Token';
-import { TokenBridgeContract } from '@aztec/noir-contracts.js/TokenBridge';
+import {TokenBridgeContract} from './artifacts/TokenBridge.js'
+import { format } from 'util';
 
-import {
-  type Account,
-  type Chain,
-  type GetContractReturnType,
-  type HttpTransport,
-  type PublicClient,
-  type WalletClient,
-  getContract,
-  toFunctionSelector,
-} from 'viem';
+const { PXE_URL = 'http://localhost:8080' } = process.env;
 
-/**
- * Deploy L1 token and portal, initialize portal, deploy a non native l2 token contract, its L2 bridge contract and attach is to the portal.
- * @param wallet - the wallet instance
- * @param walletClient - A viem WalletClient.
- * @param publicClient - A viem PublicClient.
- * @param rollupRegistryAddress - address of rollup registry to pass to initialize the token portal
- * @param owner - owner of the L2 contract
- * @param underlyingERC20Address - address of the underlying ERC20 contract to use (if none supplied, it deploys one)
- * @returns l2 contract instance, bridge contract instance, token portal instance, token portal address and the underlying ERC20 instance
- */
-export async function deployAndInitializeTokenAndBridgeContracts(
-  wallet: Wallet,
-  walletClient: WalletClient<HttpTransport, Chain, Account>,
-  publicClient: PublicClient<HttpTransport, Chain>,
-  rollupRegistryAddress: EthAddress,
-  owner: AztecAddress,
-  underlyingERC20Address?: EthAddress,
-): Promise<{
-  /**
-   * The L2 token contract instance.
-   */
-  token: TokenContract;
-  /**
-   * The L2 bridge contract instance.
-   */
-  bridge: TokenBridgeContract;
-  /**
-   * The token portal contract address.
-   */
-  tokenPortalAddress: EthAddress;
-  /**
-   * The token portal contract instance
-   */
-  tokenPortal: any;
-  /**
-   * The underlying ERC20 contract instance.
-   */
-  underlyingERC20: any;
-}> {
-  if (!underlyingERC20Address) {
-    underlyingERC20Address = await deployL1Contract(walletClient, publicClient, PortalERC20Abi, PortalERC20Bytecode);
-  }
-  const underlyingERC20 = getContract({
-    address: underlyingERC20Address.toString(),
-    abi: PortalERC20Abi,
-    client: walletClient,
-  });
+async function main() {
 
-  // deploy the token portal
-  const tokenPortalAddress = await deployL1Contract(walletClient, publicClient, TokenPortalAbi, TokenPortalBytecode);
-  const tokenPortal = getContract({
-    address: tokenPortalAddress.toString(),
-    abi: TokenPortalAbi,
-    client: walletClient,
-  });
+const logger = createDebugLogger('token');
 
-  // deploy l2 token
-  const token = await TokenContract.deploy(wallet, owner, 'TokenName', 'TokenSymbol', 18).send().deployed();
+const pxe = createPXEClient(PXE_URL);
 
-  // deploy l2 token bridge and attach to the portal
-  const bridge = await TokenBridgeContract.deploy(wallet, token.address, tokenPortalAddress).send().deployed();
+await waitForPXE(pxe, logger);
 
-  if ((await token.methods.admin().simulate()) !== owner.toBigInt()) {
-    throw new Error(`Token admin is not ${owner}`);
-  }
+const nodeInfo = await pxe.getNodeInfo();
 
-  if (!(await bridge.methods.get_token().simulate()).equals(token.address)) {
-    throw new Error(`Bridge token is not ${token.address}`);
-  }
+logger.info(format('Aztec Sandbox Info ', nodeInfo));
+ 
+const accounts = await getDeployedTestAccountsWallets(pxe);
 
-  // make the bridge a minter on the token:
-  await token.methods.set_minter(bridge.address, true).send().wait();
-  if ((await token.methods.is_minter(bridge.address).simulate()) === 1n) {
-    throw new Error(`Bridge is not a minter`);
-  }
+const adminWallet = accounts[0];
 
-  // initialize portal
-  await tokenPortal.write.initialize(
-    [rollupRegistryAddress.toString(), underlyingERC20Address.toString(), bridge.address.toString()],
-    {} as any,
-  );
+const admin = adminWallet.getAddress();
 
-  return { token, bridge, tokenPortalAddress, tokenPortal, underlyingERC20 };
+logger.info(`Deploying token contract...`);
+
+// Deploy the contract and set Alice as the admin while doing so
+const tokenContract = await TokenContract.deploy(adminWallet, admin, 'FATHOM', 'FTH', 18).send().deployed();
+
+logger.info(`Token Contract successfully deployed at address ${tokenContract.address.toShortString()}`);
+
+const bridgeContract = await TokenBridgeContract.deploy(adminWallet,tokenContract.address).send().deployed();
+
+logger.info(`Token Bridge Contract successfully deployed at address ${bridgeContract.address.toShortString()}`);
+
 }
+
+
+
+main();
